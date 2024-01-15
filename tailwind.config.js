06b9d3290548eaf171da53fc393e6262c0fc0c98
Marcus Vinicius <marcusv892@gmail.com>
2024-01-15 11:48:08 -0300

    Initializing environment application

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      light_purple: "#ddd5ea",
+      purple: "#590bd8",
+      white: "#ffffff",
+      walter_white: "#f5f5f5",
+      light_gray: "#bbbfbf",
+      red: "#fe3838",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
