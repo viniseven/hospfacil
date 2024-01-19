8527bff9355c65b15fa995748c83b308ff4037bf
Marcus Vinicius <marcusv892@gmail.com>
2024-01-18 21:43:21 -0300

    fix: insert component location

--- src/components/RecommendedDestinations/recommendedDestinations.jsx
+++ src/components/RecommendedDestinations/recommendedDestinations.jsx
@@ -1,4 +1,4 @@
-import auroraHotel from "../../assets/aurora-hotel.svg";
+import { ItemLocation } from "../ItemLocation/itemlocation";
 
 export function RecomendedDestinations() {
   return (
@@ -10,19 +10,7 @@ export function RecomendedDestinations() {
       </div>
 
       <div className="flex flex-col mt-4">
-        <img src={auroraHotel} alt="Aurora Hotel" className="rounded-lg mb-2" />
-        <div>
-          <p className="text-black">Hotel Aurora</p>
-          <p className="text-xs text-gray font-normal">
-            Rio de Janeiro, Brasil
-          </p>
-          <p className="text-xs text-gray font-normal">
-            Pŕoxima data: 13 de julho
-          </p>
-          <p className="text-xs text-gray font-normal">
-            <span className="text-purple font-semibold">R$250</span> por noite
-          </p>
-        </div>
+        <ItemLocation />
       </div>
     </div>
   );
