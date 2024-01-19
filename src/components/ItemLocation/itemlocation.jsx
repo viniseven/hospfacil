34ba07f23914df6f0d196decc5855df5d3dee980
Marcus Vinicius <marcusv892@gmail.com>
2024-01-18 21:42:29 -0300

    create: component item location

---
+++ src/components/ItemLocation/itemlocation.jsx
@@ -0,0 +1,19 @@
+import auroraHotel from "../../assets/aurora-hotel.svg";
+
+export function ItemLocation() {
+  return (
+    <div>
+      <img src={auroraHotel} alt="Aurora Hotel" className="rounded-lg mb-2" />
+      <div>
+        <p className="text-black">Hotel Aurora</p>
+        <p className="text-xs text-gray font-normal">Rio de Janeiro, Brasil</p>
+        <p className="text-xs text-gray font-normal">
+          Pŕoxima data: 13 de julho
+        </p>
+        <p className="text-xs text-gray font-normal">
+          <span className="text-purple font-semibold">R$250</span> por noite
+        </p>
+      </div>
+    </div>
+  );
+}
