1151851bcd6acabd44bf3834d4eb56908be99c0b
Marcus Vinicius <marcusv892@gmail.com>
2024-09-17 21:38:56 -0300

    fix: adjust position footer

--- frontend/src/components/Footer/footer.jsx
+++ frontend/src/components/Footer/footer.jsx
@@ -2,7 +2,7 @@ import logo from "../../assets/vector.svg";
 
 export function Footer() {
   return (
-    <div className="bg-walter_white p-5">
+    <div className="bg-walter_white p-5 absolute w-full bottom-0">
       <div className="flex items-center justify-center gap-1">
         <img src={logo} alt="Logo hospifacil" width={21} />
         <p className="text-purple text-sm font-semibold">hospfácil</p>
