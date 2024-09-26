17f3c1bce5f017047ac9a449f2d75d4f159bf334
Marcus Vinicius <marcusv892@gmail.com>
2024-09-25 22:28:05 -0300

    fix: adjust component position

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,18 +1,19 @@
-import { Header } from '../../components/Header/header';
-import { Footer } from '../../components/Footer/footer';
+import { Header } from "../../components/Header/header";
+import { Footer } from "../../components/Footer/footer";
 
-import { SearchTypes } from '../../components/SearchTypes/searchtypes';
-import { RecomendedDestinations } from '../../components/RecommendedDestinations/recommendedDestinations';
-import { SearchTrips } from '../../components/SearchTrips/searchTrips';
+import { SearchTypes } from "../../components/SearchTypes/searchtypes";
+import { RecomendedDestinations } from "../../components/RecommendedDestinations/recommendedDestinations";
+import { SearchTrips } from "../../components/SearchTrips/searchTrips";
 
 export function Home() {
   return (
     <div className="relative min-h-screen">
       <Header />
 
-      <main className="flex flex-col items-center gap-5 p-5">
+      <main className="flex flex-col items-center gap-5 py-20 px-5">
         <SearchTrips />
         <SearchTypes />
+
         <RecomendedDestinations />
       </main>
       <Footer />
