620909b796a32bef9a723d403dfeb3e72f5957c9
Marcus Vinicius <marcusv892@gmail.com>
2024-09-25 22:27:15 -0300

    fix: adjust width component

--- frontend/src/components/RecommendedDestinations/recommendedDestinations.jsx
+++ frontend/src/components/RecommendedDestinations/recommendedDestinations.jsx
@@ -2,14 +2,16 @@ import { ItemLocation } from "../ItemLocation/itemlocation";
 
 export function RecomendedDestinations() {
   return (
-    <div>
-      <div className="flex items-center gap-2">
+    <div className="flex flex-col items-center w-full overflow-auto">
+      <div className="flex items-center gap-2 w-full">
         <div className="h-[1px] w-full bg-light_gray"></div>
         <h1 className="text-gray whitespace-nowrap">Destinos Recomendados</h1>
         <div className="h-[1px] w-full bg-light_gray"></div>
       </div>
 
-      <div className="flex flex-col mt-4 gap-4">
+      <div className="flex flex-col items-center mt-4 gap-4 w-full">
+        <ItemLocation />
+        <ItemLocation />
         <ItemLocation />
         <ItemLocation />
       </div>
