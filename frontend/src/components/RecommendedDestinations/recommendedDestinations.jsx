66ae142ba7ee32ad4839c56a691eab33599feac5
Marcus Vinicius <marcusv892@gmail.com>
2024-09-26 22:07:20 -0300

    fix: adjust component overflow

--- frontend/src/components/RecommendedDestinations/recommendedDestinations.jsx
+++ frontend/src/components/RecommendedDestinations/recommendedDestinations.jsx
@@ -2,7 +2,7 @@ import { ItemLocation } from "../ItemLocation/itemlocation";
 
 export function RecomendedDestinations() {
   return (
-    <div className="flex flex-col items-center w-full overflow-auto">
+    <div className="flex flex-col items-center w-full">
       <div className="flex items-center gap-2 w-full">
         <div className="h-[1px] w-full bg-light_gray"></div>
         <h1 className="text-gray whitespace-nowrap">Destinos Recomendados</h1>
