7987cc59cfbe29ce4fa99be59b227785bd3351ba
Marcus Vinicius <marcusv892@gmail.com>
2024-09-28 18:02:42 -0300

    fix: adjust searchTrips component breakpoint md

--- frontend/src/components/SearchTrips/searchTrips.jsx
+++ frontend/src/components/SearchTrips/searchTrips.jsx
@@ -1,10 +1,10 @@
-import { Input } from "../../components/Input/input";
-import { Button } from "../../components/Button/button";
+import { Input } from '../../components/Input/input';
+import { Button } from '../../components/Button/button';
 
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import { NumericFormat } from "react-number-format";
+import { NumericFormat } from 'react-number-format';
 
 export function SearchTrips() {
   return (
@@ -13,12 +13,12 @@ export function SearchTrips() {
         Encontre sua próxima <span className="text-purple">viagem!</span>
       </h1>
 
-      <div className="flex justify-center flex-wrap gap-3.5 mt-4">
+      <div className="flex justify-center flex-wrap gap-3.5 mt-4 md:flex-nowrap">
         <Input type="text" placeholder="Onde você quer ir?" />
 
-        <div className="flex gap-4 w-full">
+        <div className="flex gap-4 md:w-3/4">
           <DatePicker
-            placeholderText="Primeira Data"
+            placeholderText="Data"
             className="border-light_gray rounded-lg border p-2 w-full"
           />
           <NumericFormat
@@ -35,7 +35,7 @@ export function SearchTrips() {
         </div>
 
         <Button
-          className="bg-purple hover:bg-dark_purple text-white w-full p-2 rounded-lg font-semibold "
+          className="bg-purple hover:bg-dark_purple text-white w-40 p-2 rounded-lg font-semibold "
           title="Pesquisar"
         />
       </div>
