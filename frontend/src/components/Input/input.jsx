d80cf45ba0546cddc412feaa66a472666116b3cb
Marcus Vinicius <marcusv892@gmail.com>
2024-09-28 18:02:02 -0300

    fix: adjust component input

--- frontend/src/components/Input/input.jsx
+++ frontend/src/components/Input/input.jsx
@@ -1,7 +1,7 @@
 export function Input({ type, placeholder, ...rest }) {
   return (
     <input
-      className="border-light_gray rounded-lg border w-full p-2"
+      className="border-light_gray rounded-lg border p-2 w-full"
       type={type}
       placeholder={placeholder}
       {...rest}
