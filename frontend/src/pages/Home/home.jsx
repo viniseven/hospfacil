73aeb8b5d7941676e4d712912c90516e632ccb2c
Marcus Vinicius <marcusv892@gmail.com>
2024-10-22 10:39:26 -0300

    fix: adjust page home responsive

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -1,16 +1,16 @@
-import { Header } from "../../components/Header/header";
-import { Footer } from "../../components/Footer/footer";
+import { Header } from '../../components/Header/header';
+import { Footer } from '../../components/Footer/footer';
 
-import { SearchTypes } from "../../components/SearchTypes/searchtypes";
-import { RecomendedDestinations } from "../../components/RecommendedDestinations/recommendedDestinations";
-import { SearchTrips } from "../../components/SearchTrips/searchTrips";
+import { SearchTypes } from '../../components/SearchTypes/searchtypes';
+import { RecomendedDestinations } from '../../components/RecommendedDestinations/recommendedDestinations';
+import { SearchTrips } from '../../components/SearchTrips/searchTrips';
 
 export function Home() {
   return (
     <div className="relative min-h-screen">
       <Header />
 
-      <main className="flex flex-col items-center gap-5 py-20 px-5">
+      <main className="flex flex-col items-center m-auto gap-5 py-20 px-5 max-w-[1360px]">
         <SearchTrips />
         <SearchTypes />
 
